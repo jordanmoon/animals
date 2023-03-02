46df179b6903b98f0e2c29611e7e60fa2866dc3c
Alan <alan.thomson@helloself.com>
2023-03-02 11:39:11 +0000

    comments

--- app.ts
+++ app.ts
@@ -1,1 +1,6 @@
-console.log("here")
+
+// 1. How many animals have hair?
+// 2. List the names of the aquatic animals
+// 3. What is the average number of times the letter "a" appears in the names of aquatic animals?
+// 4. What is the type of the data in animals.json?
+// 5. Which of the answers below is not correct and why is it not correct?
